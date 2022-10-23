e41f56d1947d4bba89c109223eabb369e2d6aacf
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2022-10-23 16:13:57 +0300

    Add button and navigation to second activity

--- HomeworkNavigationAndLifecycle/app/src/main/java/com/example/homeworknavigationandlifecycle/MainActivity.kt
+++ HomeworkNavigationAndLifecycle/app/src/main/java/com/example/homeworknavigationandlifecycle/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.homeworknavigationandlifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.homeworknavigationandlifecycle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonNextActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
