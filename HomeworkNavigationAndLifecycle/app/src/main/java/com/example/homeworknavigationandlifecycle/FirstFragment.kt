b5d2a44618781a3bf51f3fc18f3506cb9a73e992
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2022-10-23 17:42:51 +0300

    Add first fragment

---
+++ HomeworkNavigationAndLifecycle/app/src/main/java/com/example/homeworknavigationandlifecycle/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.example.homeworknavigationandlifecycle
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.homeworknavigationandlifecycle.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
