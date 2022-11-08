e4c514af678ecabaa01aed5f5d7d29ab1842fa5b
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2022-11-08 15:22:36 +0200

    Create all necessary for room database

--- HomeworkWorkWithAPI/app/src/main/java/net/gostartups/myapplication/MainActivity.kt
+++ HomeworkWorkWithAPI/app/src/main/java/net/gostartups/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package net.gostartups.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.google.android.material.snackbar.Snackbar
 import net.gostartups.myapplication.databinding.ActivityMainBinding
 import okhttp3.OkHttpClient
@@ -14,12 +16,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    var db : RoomDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        // create database instance
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "countries_app_db"
+        ).build()
+
     }
 }
