a113f18fd706efa701e1a2c19dd9c989d9764294
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2022-11-10 15:55:58 +0200

    Create save and update country in database. using database records if no internet

--- HomeworkWorkWithAPI/app/src/main/java/net/gostartups/myapplication/CountryDescriptionFragment.kt
+++ HomeworkWorkWithAPI/app/src/main/java/net/gostartups/myapplication/CountryDescriptionFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.drawToBitmap
 import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.gostartups.myapplication.databinding.FragmentCountryDescriptionBinding
 import okhttp3.OkHttpClient
 import retrofit2.Call
@@ -33,36 +36,73 @@ class CountryDescriptionFragment : Fragment() {
             .client(OkHttpClient())
             .build()
 
+        // database dao
+        val mainActivity = activity as MainActivity
+        val countryDao = mainActivity.db?.countryDao()
+
         val countryService = retrofit.create(CountryService::class.java)
         val countryRepository = CountriesRepository(countryService)
+        if (mainActivity.isNetworkConnected()) {
+            countryRepository.getCountry(countryName)
+                ?.enqueue(object : Callback<List<CountryDescription>> {
+                    override fun onResponse(
+                        call: Call<List<CountryDescription>>,
+                        response: Response<List<CountryDescription>>
+                    ) {
+                        val country = response.body() ?: return
+                        val currentCountry = country[0]
+                        binding.countryName.text = "Name : ${currentCountry.name}"
+                        binding.countryCapital.text = "Capital : ${currentCountry.capital}"
+                        binding.countryArea.text = "Area : ${currentCountry.area.toString()}"
+                        binding.countryRegion.text = "Region : ${currentCountry.region}"
+                        binding.countryPopulation.text =
+                            "Population : ${currentCountry.population.toString()}"
 
-        countryRepository.getCountry(countryName)
-            ?.enqueue(object : Callback<List<CountryDescription>> {
-                override fun onResponse(
-                    call: Call<List<CountryDescription>>,
-                    response: Response<List<CountryDescription>>
-                ) {
-                    val country = response.body() ?: return
-                    val currentCountry = country[0]
-                    binding.countryName.text = "Name : ${currentCountry.name}"
-                    binding.countryCapital.text = "Capital : ${currentCountry.capital}"
-                    binding.countryArea.text = "Area : ${currentCountry.area.toString()}"
-                    binding.countryRegion.text ="Region : ${currentCountry.region}"
-                    binding.countryPopulation.text = "Population : ${currentCountry.population.toString()}"
+                        Glide
+                            .with(binding.root)
+                            .load(currentCountry.flags.png)
+                            .centerCrop()
+                            .placeholder(R.drawable.ic_launcher_foreground)
+                            .into(binding.flag)
 
-                    Glide
-                        .with(binding.root)
-                        .load(currentCountry.flags.png)
-                        .centerCrop()
-                        .placeholder(R.drawable.ic_launcher_foreground)
-                        .into(binding.flag)
-                }
+                        GlobalScope.launch {
+                            val currentCountryEntity = CountryEntity(
+                                name = currentCountry.name,
+                                capital = currentCountry.capital,
+                                flagUrl = currentCountry.flags.png,
+                                region = currentCountry.region,
+                                population = currentCountry.population,
+                                area = currentCountry.area
+                            )
+                            countryDao?.updateCountry(currentCountryEntity)
+                        }
+                    }
 
-                override fun onFailure(call: Call<List<CountryDescription>>, t: Throwable) {
-                    Snackbar.make(binding.root, "Failed to fetch countries", Snackbar.LENGTH_LONG)
-                        .show()
-                }
-            })
+                    override fun onFailure(call: Call<List<CountryDescription>>, t: Throwable) {
+                        Snackbar.make(
+                            binding.root,
+                            "Failed to fetch countries",
+                            Snackbar.LENGTH_LONG
+                        )
+                            .show()
+                    }
+                })
+        } else {
+
+            GlobalScope.launch {
+
+                val currentCountry = countryDao?.getCountryByName(countryName)
+
+                binding.countryName.text = "Name : ${currentCountry?.name ?: ""}"
+                binding.countryCapital.text = "Capital : ${currentCountry?.capital ?: ""}"
+                binding.countryArea.text = "Area : ${currentCountry?.area?.toString() ?: ""}"
+                binding.countryRegion.text = "Region : ${currentCountry?.region ?: ""}"
+                binding.countryPopulation.text =
+                    "Population : ${currentCountry?.population?.toString() ?: ""}"
+
+                binding.flag.setImageResource(R.drawable.no_image_avavailable)
+            }
+        }
 
         binding.buttonBack.setOnClickListener {
             it.findNavController().navigate(R.id.action_countryDescription_to_Countries)
